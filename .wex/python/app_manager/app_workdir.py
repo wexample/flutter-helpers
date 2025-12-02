474f066c2e0ae7d9911b37a2a80a2b3d916f9b9f
weeger <romain.weeger@gmail.com>
2025-12-02 06:41:58 +0100

    Init

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from wexample_wex_addon_dev_flutter.workdir.php_package_workdir import FlutterPackageWorkdir
+from wexample_wex_addon_dev_flutter.workdir.flutter_package_workdir import FlutterPackageWorkdir
 
 
 class AppWorkdir(FlutterPackageWorkdir):
